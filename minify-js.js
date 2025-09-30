b073e28c1d18a59665a50f03a683718b431c84b5
laheri72 <idrislaheri72@gmail.com>
2025-10-01 02:27:56 +0530

    sss

--- minify-js.js
+++ minify-js.js
@@ -1,19 +1,18 @@
 // minify-js.js
-const { minify } = require("terser");
-const fs = require("fs");
-const path = require("path");
-const glob = require("glob");
+const { glob } = require("glob"); // destructure glob
+const { exec } = require("child_process");
 
-// Find all JS files in public/js
-glob("public/js/*.js", (err, files) => {
+glob("public/js/*.js", async (err, files) => {
   if (err) throw err;
 
-  files.forEach(async (file) => {
-    if (file.endsWith(".min.js")) return; // skip already minified
-    const code = fs.readFileSync(file, "utf8");
-    const result = await minify(code, { compress: true, mangle: true });
-    const outFile = file.replace(/\.js$/, ".min.js");
-    fs.writeFileSync(outFile, result.code, "utf8");
-    console.log(`Minified: ${outFile}`);
-  });
+  for (const file of files) {
+    const minFile = file.replace(/\.js$/, ".min.js");
+    await new Promise((resolve, reject) => {
+      exec(`npx terser "${file}" -o "${minFile}" --compress --mangle`, (error, stdout, stderr) => {
+        if (error) reject(error);
+        else resolve(stdout);
+      });
+    });
+    console.log(`Minified: ${file} → ${minFile}`);
+  }
 });
