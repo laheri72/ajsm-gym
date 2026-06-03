158fffebc67c4263ceb6336555b4b42fb9f4916e
laheri72 <idrislaheri72@gmail.com>
2026-06-03 23:56:09 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -51,6 +51,12 @@ export default defineConfig({
         staffEvaluation: resolve(__dirname, 'public/staff/evaluation.html'),
         staffCommentEntry: resolve(__dirname, 'public/staff/comment-entry.html'),
         staffeveluationlogs: resolve(__dirname, 'public/staff/evaluation-log.html'),
+        privacy: resolve(__dirname, 'public/privacy.html'),
+        googeletag: resolve(__dirname, 'public/google0cbc51477636a185.html'),
+        robots: resolve(__dirname, 'public/robots.txt'),
+        sitemap: resolve(__dirname, 'public/sitemap.xml'),
+        terms: resolve(__dirname, 'public/terms.html'),
+        
       },
     },
   },
