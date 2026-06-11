12e317255bec5ee488ae2d6124ed3b5c45d667cc
laheri72 <idrislaheri72@gmail.com>
2026-06-11 15:29:11 +0530

    remove sql and db

--- sql/migrations/2026-04-21_add_body_parts.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT 1 FROM BodyParts WHERE Name = 'Upper Body')
-BEGIN
-    INSERT INTO BodyParts (Name) VALUES ('Upper Body');
-END;
-
-IF NOT EXISTS (SELECT 1 FROM BodyParts WHERE Name = 'Lower Body')
-BEGIN
-    INSERT INTO BodyParts (Name) VALUES ('Lower Body');
-END;
